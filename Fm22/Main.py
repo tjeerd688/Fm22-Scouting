923a625f983b96b2a87bf017c428edda63e529f7
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-03-28 11:41:37 +0200

    Eerste opzet met row filters

--- Fm22/Main.py
+++ Fm22/Main.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('Fm22/Chelsea-export.csv', encoding="utf-8")
 
-print(df.to_string()) 
+df = pd.read_csv('Fm22/moneynuts.csv')
+
+df = df.replace("-", pd.np.nan)
+df = df.dropna(subset=['Gls'])
+#df_sort = df.sort_values('Gls')
+print(df)
+
+#df_sorted = df.sort_values('column_name')
+
+for index, row in df.iterrows():
+    print("index:", index, "Naam:", row['Name'], " Club:", row['Club'], "Goals:", row['Gls']) 
