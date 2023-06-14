1538111e6b8957f7aacb6aa5c0aaf3cae7b313ed
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-06-14 14:41:06 +0200

    Api werkt met de return, Code ook schoongemaakt

--- Fm22/Main.py
+++ Fm22/Main.py
@@ -8,16 +8,7 @@ df = pd.read_csv('moneynuts.csv')
 df = df.replace("-", pd.np.nan)
 df = df.dropna(subset=['Gls'])
 df = df.sort_values('Gls', ascending=False)
-print(df)
-
-#df_sorted = df.sort_values('column_name')
-
-for index, row in df.iterrows():
-    print("index:", index, "Naam:", row['Name'], " Club:", row['Club'], "Goals:", row['Gls']) 
-
 
 @app.get("/")
 async def root():
-    return{'Naam': row['Name'],'Club': row['Club'], 'Goals':row['Gls']}
-
-df.to_json(r'C:\Users\tjeer\Documents\Fm22 Scouting\Fm22\Json exports\File Name.json')
+    return(df[['Name','Club','Gls']])
