7a06e17484eee951b199cb247c8c4571155c4e1a
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-09-20 11:12:49 +0200

    Gefilterde data nu naar json omgezet en opgeslagen

--- Fm22/Main.py
+++ Fm22/Main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import json
 import pandas as pd
 import numpy as np
 app = FastAPI()
@@ -6,10 +7,10 @@ app = FastAPI()
 df = pd.read_csv('moneynuts.csv')
 
 df = df.replace("-", np.nan)
-df = df.dropna(subset=['Gls'])
-df = df.sort_values('Gls', ascending=False)
 
-print(df[['Name','Club','Gls','xG']])
-@app.get("/")
-async def root():
-   return(df[['Name','Club','Gls','xG']])
+df = df.dropna(subset=['Gls'])
+#sorting van Goals en Subsort van xG
+df = df.sort_values(['Gls','xG'], ascending=[False,False])
+df = df[['Name','Club','Gls','xG']]
+print(df)
+df.to_json(r'C:\Users\tjeer\Documents\Fm22 Scouting\Fm22\Json_exports/data_filtered.json')
