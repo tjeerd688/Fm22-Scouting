915978132706f8a95eff198c9cc4bc8ecb327cef
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-09-21 21:55:57 +0200

    Headers vernoemt

--- Fm22/Main Script/Main.py
+++ Fm22/Main Script/Main.py
@@ -4,14 +4,14 @@ import pandas as pd
 import numpy as np
 app = FastAPI()
 
-kms = input("Welke statistiek wilt U zien? Ast")
+kms = input("Welke statistiek wilt U zien? Ast ")
 df = pd.read_csv('Main script/data.csv')
 
 df = df.replace("-", np.nan)
 
-df = df.dropna(subset=['Gls'])
+df = df.dropna(subset=['Dlp'])
 #sorting van Goals en Subsort van xG
-df = df.sort_values(['Gls','xG'], ascending=[False,False])
-df = df[['Name','Club','Gls','xG',kms]]
+df = df.sort_values(['Dlp','xG'], ascending=[False,False])
+df = df[['Naam','Club','Dlp','xG',kms]]
 print(df)
 df.to_json(r'C:\Users\tjeer\Documents\Fm22 Scouting\Fm22\Json_exports/data_filtered.json')
