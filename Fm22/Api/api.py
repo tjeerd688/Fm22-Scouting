1047a15c6dede567e081025a460100861fa06e61
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-09-22 09:37:57 +0200

    api opnieuw maken

--- Fm22/Api/api.py
+++ Fm22/Api/api.py
@@ -1,29 +1,17 @@
 from typing import Union
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000"
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
 @app.get("/data")
 def Data():
 
-    file=open('Json_exports\data_filtered.json')
+    file=open('Json_exports\data_filtered.json',)
     return json.load(file)
--- Fm22/Test/Test_API.py
+++ Fm22/Test/Test_API.py
@@ -1,22 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000"
-    "http://localhost:8080"
-    "http://localhost:5500",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/")
