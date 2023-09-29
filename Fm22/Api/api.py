2a8cade4be0648a9f624fad90903703423042774
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-09-29 09:31:49 +0200

    Api response op html werkt

--- Fm22/Api/api.py
+++ Fm22/Api/api.py
@@ -1,9 +1,29 @@
 from typing import Union
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
+
 app = FastAPI()
+
+origins = ["*"]
+
+methods = ["GET", "POST", "PUT", "DELETE"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+
+)
+
+
+
 
 @app.get("/")
 def read_root():
--- Fm22/Website/script.js
+++ Fm22/Website/script.js
@@ -1,6 +1,32 @@
+fetch("http://127.0.0.1:8000/data").then((Response) => {
+    return Response.json()
+}).then((data) => {
+    console.log(data);
+})
 
-console.log("test")
-console.log(fetch('http://127.0.0.1:8000/data'))
-    .then(res => res.json())
-    .then(data => console.log(data))
+
+
+
+
+
+
+
+
+
+
+//fetch('http://localhost:8000', {
+//    mode: "cors",
+//    credentials: "include",
+//   headers: {
+//        "Content-Type": "application/json"
+//    }
+//}).then((res) => {
+//    res.json().then((data) => {
+//        console.log("Data ", data)
+//    })
+//})
+// const response = await fetch('http://127.0.0.1:8000/data');
+// const detail = await response.json();
+// var test = await fetch('http://127.0.0.1:8000/data')
+// console.log(detail)
 
