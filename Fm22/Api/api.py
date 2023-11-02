8e195e2d366acc5d65a38a64773eec53a96fc725
tjeerd688 <tjeerd.vanburen@student.aventus.nl>
2023-11-02 12:09:41 +0100

    Commentaar toegevoegd

--- Fm22/Api/api.py
+++ Fm22/Api/api.py
@@ -1,12 +1,14 @@
+#benodige libraries importeren
 from typing import Union
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
-
+#app defineren
 app = FastAPI()
 
+#cross-origin-resource-sharing aanpassen
 origins = ["*"]
 
 methods = ["GET", "POST", "PUT", "DELETE"]
@@ -18,28 +20,18 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-
-
 )
-
-
-
-
+#test response via on aangepaste http
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
+#json response van de geëxporteerde data op de  http/data url
 @app.get("/data")
+#met de api request word deze functie uitgevoerd waar de json wordt opgestuurd
 def Data():
     
     file=open(r'C:\Users\tjeer\Documents\Fm22 Scouting\Fm22\Main Script\Json_exports\DataFiltered.json',"r")
 
     return json.load(file)
 
-@app.get("/test")
-def Data():
-
-    file=open(r'C:\Users\tjeer\Documents\Fm22 Scouting\Fm22\Main Script\Json_exports\DataFiltered.json',"r")
-
-    return json.load(file)
--- Fm22/Main Script/Main.py
+++ Fm22/Main Script/Main.py
@@ -1,16 +1,22 @@
+#libraries importeren
 from fastapi import FastAPI
 import json
 import pandas as pd
 import numpy as np
-app = FastAPI()
 
+#dataframe maken van een csv bestand
 df = pd.read_csv('Main script/data.csv')
 
+#Not A Number of geen een waarde geven zodat het gefilterd kan worden
 df = df.replace("-", np.nan)
 
+#alles wat geen bruikbare data heeft eruit filteren
 df = df.dropna(subset=['Dlp'])
-print(df)
 #sorting van Goals en Subsort van xG
 df = df.sort_values(['Dlp','xG'], ascending=[False,False])
+#Naam,Club,Dlp en xG eruit filteren
 df = df[['Naam','Club','Dlp','xG']]
+#de gemanipuleerde dataframe omzetten naar een json bestand en opslaan in een export map
 df.to_json(r'C:\Users\tjeer\Documents\Fm22 Scouting\Fm22\Main Script\Json_exports/DataFiltered.json',orient='index')
+#Bericht dat het filteren en exporteren is gelukt
+print("Data gefilterd en geëxporteerd")
